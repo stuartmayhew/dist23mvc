efa26ff246b6550b5f26581299457f95224e1b1c
stuartmayhew <stumay111@gmail.com>
2015-10-08 07:21:28 -0500

    xxx

--- Dist23MVC/Dist23MVC/Controllers/MeetingsController.cs
+++ Dist23MVC/Dist23MVC/Controllers/MeetingsController.cs
@@ -9,6 +9,15 @@ using System.Web.Mvc;
 using Dist23MVC.Models;
 using Kendo.Mvc.Extensions;
 using Kendo.Mvc.UI;
+using System.Web.UI.WebControls;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.html.simpleparser;
+using System.IO;
+using System.Web.UI;
+using System.Configuration;
+using System.Data.SqlClient;
+
 
 
 namespace Dist23MVC.Controllers
@@ -16,6 +25,7 @@ namespace Dist23MVC.Controllers
     public class MeetingsController : Controller
     {
         private Dist23Data db = new Dist23Data();
+        static clsDataGetter dg;
 
         public ActionResult Meetings_Read([DataSourceRequest]DataSourceRequest request)
         {
@@ -152,6 +162,150 @@ namespace Dist23MVC.Controllers
             return RedirectToAction("MeetingsEdit");
         }
 
+        private void MakeMeetingPDF()
+        {
+            dg = new clsDataGetter(db.Database.Connection.ConnectionString);
+            SqlDataReader dr = dg.GetDataReader("EXEC sp_GetMeetingsPrint");
+            string mtgList = "";
+            //while (dr.Read())
+            //{
+            //    string mtgLine = FormatLine(dr);
+            //    //for (int i = 0; i < dr.FieldCount; i++)
+            //    //{
+            //    //    mtgLine += dr[i].ToString() + " - ";
+            //    //}
+            //    mtgList += mtgLine;
+            //}
+            //dg.KillReader(dr);
+            FillPDFForm(Server.MapPath("~\\upload") + "\\district_23.pdf", Server.MapPath("~\\upload")+ "\\district_23101.pdf", dr);
+            DisplayDocumentOrErrorPage(Server.MapPath("~\\upload") + "\\district_23101.pdf");
+        }
+
+        public static void FillPDFForm(string spath, string destpath, SqlDataReader dr)
+        {
+            using (var fs = new FileStream(destpath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite))
+            {
+                var reader = new iTextSharp.text.pdf.PdfReader(new RandomAccessFileOrArray(spath), null);
+                replaceFields(reader, fs, dr);
+            }
+        }
+
+        private static void replaceFields(iTextSharp.text.pdf.PdfReader reader, FileStream fs, SqlDataReader dr)
+        {
+            using (var pdfStamper = new PdfStamper(reader, fs))
+            {
+                int i = 0;
+                while (dr.Read())
+                {
+                    i++;
+
+                    if (i > 1)
+                        break;
+                    TextField field = new TextField(pdfStamper.Writer, new iTextSharp.text.Rectangle(40, 500, 360, 530), "Day" + i.ToString());
+                    pdfStamper.AddAnnotation(field.GetTextField(), 1);
+                    pdfStamper.AcroFields.SetField("Day" + i.ToString(), FormatField(dr["Day"].ToString(), "Day"));
+
+
+
+
+                    //pdfStamper.AcroFields.SetField("Time" + i.ToString(), FormatField(dr["Time"].ToString(), "Time"));
+                    //pdfStamper.AcroFields.SetField("Type" + i.ToString(), FormatField(dr["Type"].ToString(), "Type"));
+                    //pdfStamper.AcroFields.SetField("Topic" + i.ToString(), FormatField(dr["Topic"].ToString(), "Topic"));
+                    //pdfStamper.AcroFields.SetField("Group" + i.ToString(), FormatField(dr["aaGroup"].ToString(), "aaGroup"));
+                    //pdfStamper.AcroFields.SetField("Location" + i.ToString(), FormatField(dr["Location"].ToString(), "Location"));
+                }
+                pdfStamper.FormFlattening = true;
+                pdfStamper.Close();
+                reader.Close();
+            }
+
+        }
+
+        public static PdfStamper AddField(string source, string dest)
+        {
+            PdfReader reader = new PdfReader(source);
+
+            FileStream output = new FileStream(dest, FileMode.Create, FileAccess.Write);
+
+            PdfStamper stamp = new PdfStamper(reader, output);
+
+            return stamp;
+
+
+        }
+
+        protected void DisplayDocumentOrErrorPage(string FilePath)
+        {
+            if (String.IsNullOrWhiteSpace(FilePath))
+            {
+                //RedirectToErrorPage();
+            }
+            else
+            {
+                try
+                {
+                    byte[] pdfBytes = System.IO.File.ReadAllBytes(FilePath);
+                    if (pdfBytes.GetLength(0) == 0)
+                    {
+                        throw new Exception();
+                    }
+                    Response.ClearContent();
+                    Response.ClearHeaders();
+
+                    Response.AddHeader("Content-disposition", "attachment; filename=" +
+                                       FilePath);
+                    Response.ContentType = "application/octet-stream";
+                    Response.BinaryWrite(pdfBytes);
+                    Response.End();
+                }
+                catch (Exception ex)
+                {
+                    //RedirectToErrorPage();
+                }
+            }
+        }
+        public void DownloadMeetings()
+        {
+            MakeMeetingPDF();
+        }
+
+        private string FormatLine(SqlDataReader dr)
+        {
+            string mtgLine = "";
+            for (int i = 0; i < dr.FieldCount; i++)
+            {
+                mtgLine += FormatField(dr[i].ToString(), dr.GetName(i));
+            }
+
+            return mtgLine + "\n\r";
+
+        }
+        private static string FormatField(string fieldData,string fieldName)
+        {
+            return fieldData;
+            //bool mustSplit = false;
+            //int maxLength = dg.GetScalarInteger("SELECT MAX(LEN(" + fieldName + ")) FROM meetings");
+            //if (maxLength > 15 && fieldData.Contains(" "))
+            //    mustSplit = true;
+
+            //string newStr = "";
+            //if (mustSplit)
+            //{
+            //    string[] strs = fieldData.Split(' ');
+            //    int wordCount = strs.Length;
+            //    decimal splitDec = wordCount / 2;
+            //    int splitSpot = ((int)Math.Floor(splitDec) - 1);
+            //    strs[splitSpot] += "\n\r";
+            //    for (int x = 0;x < wordCount; x++)
+            //    {
+            //        newStr += strs[x];
+            //    }
+            
+            //}
+            //else
+            //    newStr = fieldData.PadRight(maxLength);
+            //return newStr;
+        }
         protected override void Dispose(bool disposing)
         {
             if (disposing)
---
+++ Dist23MVC/Dist23MVC/Models/clsDataGetter.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Dist23MVC.Models
+{
+    class clsDataGetter
+    {
+        public SqlConnection conn;
+        string cnStr;
+
+        public clsDataGetter(string connStr)
+        {
+            conn = new System.Data.SqlClient.SqlConnection(connStr);
+            cnStr = connStr;
+        }
+
+        public System.Data.SqlClient.SqlDataReader GetDataReader(string sql,SqlConnection newConn=null)
+        {
+            System.Data.SqlClient.SqlDataReader dr = null;
+            if (newConn == null)
+            {
+                newConn = conn;
+            }
+
+            if (newConn.State != ConnectionState.Open)
+            {
+                newConn.Open();
+            }
+
+            System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand(sql, newConn);
+            cmd.CommandTimeout = 3600;
+            try
+            {
+                dr = cmd.ExecuteReader();
+            }
+            catch
+            {
+                newConn.Close();
+            }
+            return dr;
+        }
+
+        public void KillReader(SqlDataReader dr) 
+        {
+            dr.Close();
+            dr.Dispose();
+        }
+        public DataSet GetDataSet(string sql)
+        {
+            System.Data.SqlClient.SqlConnection conn3 = new System.Data.SqlClient.SqlConnection(cnStr);
+            System.Data.SqlClient.SqlDataAdapter adapt = new System.Data.SqlClient.SqlDataAdapter(sql, conn3);
+            DataSet ds = new DataSet();
+            adapt.Fill(ds);
+            conn3.Close();
+            conn3.Dispose();
+            conn3 = null;
+            return ds;
+        }
+
+        public int GetScalarInteger(string sql)
+        {
+            int x = -1;
+            System.Data.SqlClient.SqlConnection conn3 = new System.Data.SqlClient.SqlConnection(cnStr);
+            conn3.Open();
+            SqlCommand cmd = new SqlCommand(sql, conn3);
+            x = (int)cmd.ExecuteScalar();
+            conn3.Close();
+            conn3.Dispose();
+            conn3 = null;
+            return x;
+        }
+
+        public bool GetScalarBoolean(string sql)
+        {
+            bool x;
+            System.Data.SqlClient.SqlConnection conn3 = new System.Data.SqlClient.SqlConnection(cnStr);
+            conn3.Open();
+            SqlCommand cmd = new SqlCommand(sql, conn3);
+            x = (bool)cmd.ExecuteScalar();
+            conn3.Close();
+            conn3.Dispose();
+            conn3 = null;
+            return x;
+        }
+
+        public string GetScalarString(string sql)
+        {
+            string x = "";
+            System.Data.SqlClient.SqlConnection conn3 = new System.Data.SqlClient.SqlConnection(cnStr);
+            conn3.Open();
+            SqlCommand cmd = new SqlCommand(sql, conn3);
+            object result = cmd.ExecuteScalar();
+            if (result.ToString() == "")
+            {
+                x = "";
+            }
+            else
+            {
+                x = (string)result;
+            }
+            conn3.Close();
+            conn3.Dispose();
+            conn3 = null;
+            return x;
+        }
+
+        public bool HasData(string sql,SqlConnection newConn=null)
+        {
+            System.Data.SqlClient.SqlDataReader dr;
+            if (newConn == null)
+            {
+                newConn = conn;
+            }
+
+            if (newConn.State != System.Data.ConnectionState.Open)
+            {
+                newConn.Open();
+            }
+
+            SqlCommand cmd = new SqlCommand(sql, newConn);
+            cmd.CommandTimeout = 3600;
+            try
+            {
+                dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    dr.Close();
+                    newConn.Close();
+                    return true;
+                }
+                else
+                {
+                    dr.Close();
+                    newConn.Close();
+                    return false;
+                }
+            }
+            catch
+            {
+                newConn.Close();
+                return false;
+            }
+        }
+
+        public string RunCommand(string sql)
+        {
+            if (conn.State != System.Data.ConnectionState.Open)
+            {
+                conn.Open();
+            }
+
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.CommandTimeout = 6000000;
+            try
+            {
+                cmd.ExecuteNonQuery();
+                return "";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+    }
+}
+
+
+

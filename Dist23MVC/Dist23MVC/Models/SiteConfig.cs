fec6f35a6689f17828b9b4b0ce9cc19b9618ab6a
stuartmayhew <stumay111@gmail.com>
2016-01-14 07:36:33 -0600

    site config

---
+++ Dist23MVC/Dist23MVC/Models/SiteConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dist23MVC.Models
+{
+    public class SiteConfig
+    {
+        [Key]
+        public int DistKey { get; set; }
+        public string DistURL { get; set; }
+        public string DistStyle { get; set; }
+        public string BannerTitle { get; set; }
+        public string BannerSubTitle { get; set; }
+        public string HotlinePh { get; set; }
+        public string AltHotline { get; set; }
+        public string AltHotlineMsg { get; set; }
+        public string DomainName { get; set; }
+    }
+}

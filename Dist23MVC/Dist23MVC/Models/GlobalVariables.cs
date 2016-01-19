e66fbd21b8a14d18daa4e1c56d2636fe5eb175e7
stuartmayhew <stumay111@gmail.com>
2016-01-19 07:52:46 -0600

    global

---
+++ Dist23MVC/Dist23MVC/Models/GlobalVariables.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dist23MVC.Models
+{
+    public static class GlobalVariables
+    {
+        //public static string DistNumber { get; set; }
+        //public static int DistKey { get; set; }
+        //public static string BannerText { get; set; }
+        //public static string BannerSubText { get; set; }
+        //public static string StyleSheet { get; set; }
+        //public static string SiteName { get; set; }
+        //public static string DomainName { get; set; }
+        //public static string Hotline { get; set; }
+        //public static string AltHotline { get; set; }
+        //public static string AltHoltlineMsg { get; set; }
+        //public static bool isMobile { get; set; }
+        public static string DistNumber = "23";
+        public static int DistKey = 23;
+        public static string BannerText = "Alcoholics Anonymous of the Eastern Shore, Baldwin County, AL";
+        public static string BannerSubText = "District 23 - Serving Bay Minette, Fairhope, Spanish Fort and Daphne";
+        public static string StyleSheet = "~/Content/Site23.css";
+        public static string SiteName = "AA District 23";
+        public static string DomainName = "easternshoreaa.org";
+        public static string Hotline = "(251)301-6773";
+        public static string AltHotline = "(251) 301-6882 ";
+        public static string AltHoltlineMsg = "Si tienes problemas con alcohol y no puedes dejar de beber. Llamanos al";
+        public static bool isMobile = false;
+    }
+}

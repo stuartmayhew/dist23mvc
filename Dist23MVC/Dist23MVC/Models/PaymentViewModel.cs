1fb9916b09d5a89ec82bd927ba90b6b5508d8697
stuartmayhew <stumay111@gmail.com>
2016-03-22 17:39:55 -0500

    payments

---
+++ Dist23MVC/Dist23MVC/Models/PaymentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dist23MVC.Models
+{
+    public class PaymentViewModel
+    {
+        [Key]
+        public int PaymentKey { get; set; }
+        public Payments payment { get; set; }
+        public string PaypalButton { get; set; }
+    }
+}
